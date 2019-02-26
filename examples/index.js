0ac0849eb434882391972d6663903558100ad9d3
iwburns <iburns@quantumedgetechnology.com>
2019-02-25 19:18:52 -0600

    move example code into examples directory

---
+++ examples/index.js
@@ -0,0 +1,11 @@
+const { LazyIter } = require('../dist/index');
+
+const iter = LazyIter.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+const total = iter
+  .filter(x => x % 2 === 0)
+  .take(5)
+  .map(x => x * 3)
+  .reduce((sum, curr) => sum + curr, 0);
+
+console.log(total);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-// export { default as LazyIter } from './iter';
-
-import LazyIter from './iter';
-
-const iter = LazyIter.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-
-const total = iter
-  .filter(x => x % 2 === 0)
-  .take(5)
-  .map(x => x * 3)
-  .reduce((sum, curr) => sum + curr, 0);
-
-console.log(total);
+export { default as LazyIter } from './iter';
