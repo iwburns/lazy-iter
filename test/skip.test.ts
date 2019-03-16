4a8d4f093710933744011ee8d861bf11eceac210
iwburns <iburns@quantumedgetechnology.com>
2019-03-15 23:59:04 -0500

    add SkipIter tests

---
+++ test/skip.test.ts
@@ -0,0 +1,149 @@
+import { LazyIter } from '../src/index';
+import util from './util';
+
+const src = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function getIter(skip: number) {
+  return {
+    iter: LazyIter.from(src).skip(skip),
+    values: src.slice(skip)
+  };
+}
+
+describe('SkipIter', () => {
+  describe('next', () => {
+    it('should iterate over an array', () => {
+      const { iter, values } = getIter(2);
+      util.expectToIterateOverValues(iter, values);
+    });
+  });
+
+  describe('nth', () => {
+    it('should return the nth item in the iterator', () => {
+      const { iter, values } = getIter(2);
+
+      expect(iter.nth(2).value).toEqual(5);
+
+      // nth(n) is like skipping (n + 1)
+      util.expectToIterateOverValues(iter, values.slice(3));
+    });
+  });
+
+  describe('toArray', () => {
+    it('should consume the iterator and return an array of values', () => {
+      const { iter, values } = getIter(2);
+      const arr = iter.toArray();
+
+      expect(arr).toEqual(values);
+      expect(iter.next().done).toBeTruthy();
+    });
+
+    it('should only return items that have not been iterated over yet', () => {
+      const { iter, values } = getIter(2);
+
+      expect(iter.next().value).toBe(3);
+
+      const arr = iter.toArray();
+
+      expect(arr).toEqual(values.slice(1));
+      expect(iter.next().done).toBeTruthy();
+    });
+  });
+
+  describe('forEach', () => {
+    it('should consume the iterator and call the given function on each item', () => {
+      const { iter, values } = getIter(2);
+      const expectedIndexes = values.map((val, i) => i);
+
+      const vals: Array<number> = [];
+      const indexes: Array<number> = [];
+
+      iter.forEach((val, index) => {
+        vals.push(val);
+        indexes.push(index);
+      });
+
+      expect(vals).toEqual(values);
+      expect(indexes).toEqual(expectedIndexes);
+    });
+  });
+
+  describe('reduce', () => {
+    it('should consume the iterator and return the "reduced" result', () => {
+      const { iter, values } = getIter(2);
+
+      const reducer = (acc: number, curr: number) => acc + curr;
+
+      const reduced = iter.reduce(reducer, 0);
+      const expected = values.reduce(reducer, 0);
+
+      expect(reduced).toBe(expected);
+      expect(iter.next().done).toBeTruthy();
+    });
+  });
+
+  describe('skip', () => {
+    it('should skip x number of elements and continue iterating', () => {
+      const { iter, values } = getIter(2);
+      util.expectToIterateOverValues(iter.skip(2), values.slice(2));
+    });
+  });
+
+  describe('take', () => {
+    it('should iterate over x number of elements and stop', () => {
+      const { iter, values } = getIter(2);
+      util.expectToIterateOverValues(iter.take(3), values.slice(0, 3));
+    });
+  });
+
+  describe('filter', () => {
+    it('should filter out items that do not pass the predicate test', () => {
+      const { iter, values } = getIter(2);
+      const pred = (x: number) => x % 2 === 0;
+      util.expectToIterateOverValues(iter.filter(pred), values.filter(pred));
+    });
+  });
+
+  describe('map', () => {
+    it('should map each item with the mapping function and iterate over the new values', () => {
+      const { iter, values } = getIter(2);
+      const mapper = (x: number) => x * 10;
+      util.expectToIterateOverValues(iter.map(mapper), values.map(mapper));
+    });
+  });
+
+  describe('zip', () => {
+    it('should iterate over two different iterables at the same time', () => {
+      const { iter, values } = getIter(2);
+      const other = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+      util.expectToIterateOverValues(iter.zip(other), util.getPairs(values, other));
+    });
+
+    it('should stop iterating as soon as one iterable is done (left first)', () => {
+      const { iter, values } = getIter(2);
+      const other = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'];
+      util.expectToIterateOverValues(iter.zip(other), util.getPairs(values, other));
+    });
+
+    it('should stop iterating as soon as one iterable is done (right first)', () => {
+      const { iter, values } = getIter(2);
+      const other = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+      util.expectToIterateOverValues(iter.zip(other), util.getPairs(values, other));
+    });
+  });
+
+  describe('chain', () => {
+    it('should iterate over two different iterables one after the other', () => {
+      const other = ['a', 'b', 'c'];
+      const { iter, values } = getIter(2);
+      util.expectToIterateOverValues(iter.chain(other), [...values, ...other]);
+    });
+  });
+
+  describe('enumerate', () => {
+    it('should iterate over an iterable while providing an index', () => {
+      const { iter, values } = getIter(2);
+      util.expectToIterateOverValues(iter.enumerate(), values.map((val, i) => [val, i]));
+    });
+  });
+});
--- test/util.ts
+++ test/util.ts
@@ -7,6 +7,23 @@ export function expectToIterateOverValues<T, U>(iter: LazyIter<T, U>, values: Ar
   expect(iter.next().done).toBeTruthy();
 }
 
+export function getPairs<T, U>(arr1: Array<T>, arr2: Array<U>): Array<[T, U]> {
+  const output: Array<[T, U]> = [];
+
+  if (arr1.length > arr2.length) {
+    arr2.forEach((val, i) => {
+      output.push([arr1[i], val]);
+    });
+  } else {
+    arr1.forEach((val, i) => {
+      output.push([val, arr2[i]]);
+    });
+  }
+
+  return output;
+}
+
 export default {
   expectToIterateOverValues,
+  getPairs,
 };
